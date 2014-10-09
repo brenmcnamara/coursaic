36bed89895c71a101be35cd2fb2983c9053cca2a
Brendan McNamara <bahai9@live.com>
2014-10-09 16:38:34 -0500

    Moves markup from home page to react js

--- public/views/home.js
+++ public/views/home.js
@@ -12,6 +12,7 @@
  *  - React
  *  - View namespace
  *  - Components in header.js
+ *  - Components in widgets.js
  */
 
 /**
@@ -21,13 +22,108 @@
  * elements on the home page will exist inside
  * this element.
  */
-View.Root_Home = React.createClass({
+View.Home_Root = React.createClass({
     render: function() {
         return (
             <div className="main">
                 <View.Header />
                 <View.Header_Fill />
+                <View.Home_Img />
+                <View.Search />
+                <View.Home_Content />
             </div>
         );
     }
 });
+
+
+/**
+ * Home_Img
+ *
+ * The decorative image on the home
+ * page.
+ */
+View.Home_Img = React.createClass({
+    render: function() {
+        return (
+            <div className="home-image"></div>
+        );
+    }
+});
+
+
+/**
+ * Search
+ *
+ * The search bar for looking up courses.
+ */
+View.Search = React.createClass({
+    render: function() {
+        return (
+            <div className="home-search">
+                <div className="search">
+                    <input type="text"
+                           placeholder="Search for Vanderbilt Courses..."
+                           className="search__input" />
+
+                    <img className="search__submit" src="./img/icons/search.png" />
+                </div>
+            </div>
+        );
+    }
+});
+
+
+/**
+ * Home_Content
+ *
+ * All the major content on the
+ * home page, including side navigation
+ * and courses to lookup.
+ */
+View.Home_Content = React.createClass({
+    render: function() {
+        return (
+            <div className="home-content content">
+                <View.Home_SideNav />
+                <View.Home_Body />
+            </div>
+        );
+    }
+});
+
+
+/**
+ * Home_SideNav
+ *
+ * The Side Navigation on the home
+ * page.
+ */
+View.Home_SideNav = React.createClass({
+    render: function() {
+        return (
+            <div className="content__nav">
+                <View.MainOptions />
+                <View.Divide />
+                <View.MyCourses />
+            </div>
+        );
+    }
+});
+
+/**
+ * Home_Body
+ *
+ * The central markup inside the content
+ * tag, displayed on the home page.
+ */
+View.Home_Body = React.createClass({
+    render: function() {
+        return (
+            <div className="home-content__courses">
+                <View.CourseGrid />
+            </div>
+        );
+    }
+});
+
---
+++ public/views/widgets.js
@@ -0,0 +1,173 @@
+/** @jsx React.DOM */
+
+/**
+ * widgets.js
+ *
+ * All elements that are supported across
+ * different web pages.
+ */
+
+/*
+ * Dependencies:
+ *  - React
+ *  - View namespace
+ */
+
+/**
+ * MainOption
+ *
+ * Main Options for the user to take, such as
+ * creating a class or creating an exam.
+ */
+View.MainOptions = React.createClass({
+    render: function() {
+
+        var createCourseStyle = {
+                height: '30px',
+                margin: '-15px 0 0 9px'
+            },
+            inviteFriendsStyle = {
+                height: '28px',
+                margin: '-14px 0 0 0'
+            },
+            changeSchoolStyle = {
+                height: '28px',
+                margin: '-14px 0 0 6px'
+            };
+
+        return (
+            <ul className="main-options">
+                <li className="main-options__item">
+                    <img src="./img/icons/book.png"
+                         style={createCourseStyle}
+                         className="main-options__item__icon" />
+                    <div className="main-options__item__text">Create Course</div>
+                </li>
+
+                <li className="main-options__item">
+                    <img src="./img/icons/group.png"
+                         style={inviteFriendsStyle}
+                         className="main-options__item__icon" />
+
+                    <div className="main-options__item__text">Invite Friends</div>
+                </li>
+
+                <li className="main-options__item">
+                    <img src="./img/icons/building.png"
+                         style={changeSchoolStyle} 
+                         className="main-options__item__icon" />
+                    <div className="main-options__item__text">Change School</div>
+                </li>
+            </ul>
+        );
+    }
+});
+
+
+/**
+ * MyCourses
+ *
+ * A list of all courses the user
+ * is currently enrolled in. Serves
+ * for quick, easy navigation to
+ * these courses.
+ */
+View.MyCourses = React.createClass({
+    render: function() {
+        return (
+            <section className="category my-courses">
+                <div className="category__title">My Courses</div>
+                <ul className="category__list">
+                    <li>CS 101</li>
+                    <li>CS 201</li>
+                    <li>MATH 247</li>
+                    <li>ENGL 210A</li>
+                </ul>
+            </section>
+        );
+    }
+});
+
+
+/**
+ * Divide
+ *
+ * A divide element used to separate
+ * components.
+ */
+View.Divide = React.createClass({
+    render: function() {
+        return (
+            <div className="divide"></div>
+        );
+    }
+});
+
+
+/**
+ * CourseGrid
+ *
+ * A grid of courses.
+ */
+View.CourseGrid = React.createClass({
+    render: function() {
+        return (
+            <div className="course-grid">
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+                <View.CourseInfo />
+            </div>
+        );
+    }
+});
+
+
+/**
+ * CourseInfo
+ *
+ * The information for a course displayed
+ * after a search.
+ */
+View.CourseInfo = React.createClass({
+    render: function() {
+
+        var courseHeaderStyle = {
+            background: '#417505'
+        };
+        // TODO: Separate out tags that are specific
+        // to a page (i.e. home-content__course__grid__course).
+        return (
+            <div className="home-content__courses__grid__course course-info">
+                <header className="course-info__header" style={courseHeaderStyle}>CS 101</header>
+                <div className="course-info__body">
+                    <div className="course-info__body__text">Introduction to Computer Science</div>
+                </div>
+                <footer className="course-info__footer">12 enrolled</footer>
+            </div>
+        );
+    }
+});
+
