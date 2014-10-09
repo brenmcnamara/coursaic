e2ca73bcff9d63ff0b8de199f8dc973c87ecf00a
Brendan McNamara <bahai9@live.com>
2014-10-09 16:45:19 -0500

    Adds react to course.html

---
+++ public/views/course.js
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+/**
+ * home.js
+ *
+ * All React Components targeted
+ * specifically for the home page.
+ */
+
+/*
+ * Dependencies:
+ *  - React
+ *  - View namespace
+ *  - Components in header.js
+ *  - Components in widgets.js
+ */
+
+
+/**
+ * Course_Root
+ *
+ * The root element on the Course
+ * page.
+ */
+View.Course_Root = React.createClass({
+    render: function() {
+        return (
+            <div className="main">
+            </div>
+        );
+    }
+});
+
+
+
+
