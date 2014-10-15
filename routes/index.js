97755429949814bae432d0668d44900019f3c16c
Brendan McNamara <bahai9@live.com>
2014-10-15 16:37:25 -0500

    Changes structure to support ejs files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index.html');
+exports.root = function(req, res) {
+    res.redirect('/home');
+};
+
+
+exports.index = function(req, res) {
+  res.render('index');
+};
+
+
+exports.course = function(req, res) {
+    res.render('course');
 };
