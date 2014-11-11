bc7ec6ccef3dd30f515f3c1b139caf0d35bf9c8e
Brendan McNamara <bahai9@live.com>
2014-11-11 15:53:22 -0600

    Removes mock features that are to be removed in version 1

--- public/components/course.react.js
+++ public/components/course.react.js
@@ -57,29 +57,6 @@ View.Course_Dashboard = React.createClass({
 
         return (
             <div className="dashboard">
-                <ul className="dashboard__course-logistics">
-                    <li className="dashboard__course-logistics__item">2 exams</li>
-                    <li className="dashboard__course-logistics__item">12 people enrolled</li>
-                </ul>
-
-                <ul className="dashboard__profile-grid profile-grid" style={ profileGridStyle }>
-                    <li className="profile-grid__row__item profile-pic--square">
-                        <img src="/img/temp/bunny-profile.png" />
-                    </li>
-                    <li className="profile-grid__row__item profile-pic--square">
-                        <img src="/img/temp/frat-profile.png" />
-                    </li>
-                    <li className="profile-grid__row__item profile-pic--square">
-                        <img src="/img/temp/dog-profile.png" />
-                    </li>
-                    <li className="profile-grid__row__item profile-pic--square">
-                        <img src="/img/temp/monkey-profile.png" />
-                    </li>
-                    <li className="profile-grid__row__item profile-pic--square">
-                        <img src="/img/temp/tiger-profile.png" />
-                    </li>
-                    <li className="profile-grid__row__item member-count--square">+7</li>
-                </ul>
             </div>
         );
     }
@@ -102,13 +79,6 @@ View.Course_Summary = React.createClass({
                     <span className="course-summary__field__key">Field: </span>
                     <span className="course-summary__field__value">{ course.get('field').get('name') }</span>
                 </p>
-
-                <div className="profile-pic--circle--bordered course-summary__creator-profile">
-                    <img src="/img/temp/monkey-profile.png" />
-                </div>
-                <div className="course-summary__creator-info">Course created by
-                    <span className="user-name"> Monkey Joe</span> on Sept. 20, 2013.
-                </div>
             </div>
         );
     }
@@ -180,17 +150,6 @@ View.Course_Content_Body = React.createClass({
                 <div className="content__body__wrapper">
                     <h1 className="content__body__title">Exam 1</h1>
                     <div className="exam">
-                        <div className="exam__creator-info">
-                            <div className="exam__creator-info__pic">
-                                <div className="profile-pic--circle--bordered">
-                                    <img src="/img/temp/frat-profile.png" />
-                                </div>
-                            </div>
-                            
-                            <span className="exam__creator-info__text">
-                                Exam created by <span className="user-name">Frat Star</span> on Sept. 22, 2013.
-                            </span>
-                        </div>
                         <div className="exam__my-questions">
                             <div className="exam__my-questions__title">My Questions</div>
                             <ul className="exam__my-questions__question-list question-list">
