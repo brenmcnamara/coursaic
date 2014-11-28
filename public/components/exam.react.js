b5a0e61963f2e6952a511537f0b7c18b744708f9
Brendan McNamara <bahai9@live.com>
2014-11-28 13:12:04 -0600

    Renders exam root

---
+++ public/components/exam.react.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+/*
+ * exam.react.js
+ *
+ * The components for the exam page.
+ */
+
+View.Exam_Root = React.createClass({
+
+    render: function() {
+        return <div>Hello, World from Exam Page.</div>
+    }
+
+
+});
