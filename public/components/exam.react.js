58f42d2ce4c9f2c95fc9b5065e5ab345c73fe528
Brendan McNamara <bahai9@live.com>
2014-11-28 17:43:07 -0600

    Separates out exam mock to separate react components

--- public/components/exam.react.js
+++ public/components/exam.react.js
@@ -13,86 +13,87 @@ View.Exam_Root = React.createClass({
             <div className="main">
                 <View.Header isOpaque={ false } />
                 <View.Header_Fill isOpaque={ false } />
-                <div className="exam">
-                    <h1 className="exam__title">Exam 1</h1>
-                    <ul className="exam__question-list">
-                        <li className="question-item--multi-choice">
-                            <div className="question-item__question">Why is the sky so blue?</div>
-                            <ul className="question-item--multi-choice__options">
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="question-item--multi-choice">
-                            <div className="question-item__question">Why is the sky so blue?</div>
-                            <ul className="question-item--multi-choice__options">
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="question-item--multi-choice">
-                            <div className="question-item__question">Why is the sky so blue?</div>
-                            <ul className="question-item--multi-choice__options">
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="question-item--multi-choice">
-                            <div className="question-item__question">Why is the sky so blue?</div>
-                            <ul className="question-item--multi-choice__options">
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                                <li className="question-item--multi-choice__options__item">
-                                    <input type="radio" />Option 1
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <div className="button-wrapper exam__button-wrapper">
-                        <button type="button" className="button">Submit</button>
-                    </div>
-                </div>
+                <View.Exam_Form />
             </div>
         );
     }
-/*
-                
-                */
+
+
 });
+
+
+View.Exam_Form = React.createClass({
+
+    render: function() {
+        return (
+            <div className="exam">
+                <h1 className="exam__title">Exam 1</h1>
+                <View.Exam_Form_Question_List />
+                <View.Exam_Form_Buttons />
+            </div>
+        );
+    }
+
+
+});
+
+
+View.Exam_Form_Question_List = React.createClass({
+
+    render: function() {
+        return (
+            <ul className="exam__question-list">
+                <View.Exam_Form_Question />
+                <View.Exam_Form_Question />
+                <View.Exam_Form_Question />
+                <View.Exam_Form_Question />
+            </ul>
+        );
+    }
+
+
+});
+
+
+View.Exam_Form_Question = React.createClass({
+
+    render: function() {
+        return (
+            <li className="question-item--multi-choice">
+                <div className="question-item__question">Why is the sky so blue?</div>
+                <ul className="question-item--multi-choice__options">
+                    <li className="question-item--multi-choice__options__item">
+                        <input type="radio" />Option 1
+                    </li>
+                    <li className="question-item--multi-choice__options__item">
+                        <input type="radio" />Option 1
+                    </li>
+                    <li className="question-item--multi-choice__options__item">
+                        <input type="radio" />Option 1
+                    </li>
+                    <li className="question-item--multi-choice__options__item">
+                        <input type="radio" />Option 1
+                    </li>
+                </ul>
+            </li>
+        );
+    }
+
+
+});
+
+
+View.Exam_Form_Buttons = React.createClass({
+
+    render: function() {
+        return (
+            <div className="button-wrapper exam__button-wrapper">
+                <button type="button" className="button">Submit</button>
+                <button type="button" className="button">Cancel</button>
+            </div>
+        );
+    }
+
+
+});
+
