6a2c9457de8b96e36c42b4aa9ad67c1fa8d32f0b
Brendan McNamara <bahai9@live.com>
2014-12-05 21:39:17 -0600

    Sets CREATE_COURSE mode in the page store

--- public/js/Stores/PageStore.js
+++ public/js/Stores/PageStore.js
@@ -35,10 +35,88 @@ var PageStore = (function() {
                                             throw error;
                                         });
                 };
-                    
+            case Action.Name.CREATE_COURSE:
+                return function(payload) {
+                    return Dispatcher.waitFor([ CourseStore.dispatcherIndex ])
+                                     .then(
+                                        // Success.
+                                        function() {
+                                            if (self._currentMode !== PageStore.Mode.CREATE_COURSE) {
+                                                throw new Error("Cannot create a course " +
+                                                                "when not in CREATE_COURSE mode.");
+                                            } 
+                                        },
+                                        // Error.
+                                        function(error) {
+                                            throw error;
+                                        });
+
+                };
+            case Action.Name.ENTER_CREATE_COURSE_MODE:
+                return function(payload) {
+                    return new Promise(function(resolve, reject) {
+                        if (self._currentMode) {
+                            throw new Error("Attempting to change to mode: " +
+                                            PageStore.Mode.CREATE_COURSE + 
+                                            " when already in mode " + self._currentMode);
+                        }
+                        self._currentMode = PageStore.Mode.CREATE_COURSE;
+                        // TODO (brendan): Copy the payload.
+                        self._currentPayload = payload;
+                        resolve();
+                    });
+                };
+            case Action.Name.CANCEL_CREATE_COURSE:
+                return function(payload) {
+                    return new Promise(function(resolve, reject) {
+                        if (self._currentMode !== PageStore.Mode.CREATE_COURSE) {
+                            throw new Error("Cannot cancel create course " +
+                                            "when not in CREATE_COURSE mode.");
+                        }
+                        self._currentMode = null;
+                        self._currentPayload = null;
+                        resolve();
+                    });
+                };
         };
     };
+
+    /**
+     * Get the current mode for the page.
+     *
+     * @method mode
+     *
+     * @return {String} The current mode of the page.
+     *  If there is no current mode, it will return null.
+     */
+    StoreClass.prototype.currentMode = function() {
+        return this._currentMode || null;
+    };
+
+
+    /**
+     * Get the payload for the current mode.
+     *
+     * @method payload
+     *
+     * @return {Object} The payload for the current
+     *  mode. If there is no mode, this will return null.
+     */
+    StoreClass.prototype.currentPayload = function() {
+        return this._currentPayload || null;
+    };
+
 
     return new StoreClass();
 
 }());
+
+PageStore.Mode = {
+    CANCEL_TAKE_EXAM: 'CANCEL_TAKE_EXAM',
+    CREATE_COURSE: 'CREATE_COURSE',
+    CREATE_EXAM: 'CREATE_EXAM',
+    CREATE_QUESTION: 'CREATE_QUESTION',
+    DELETE_QUESTION: 'DELETE_QUESTION',
+    EDIT_QUESTION: 'EDIT_QUESTION'
+};
+
