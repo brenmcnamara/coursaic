7726f272471908478b606fe4d5d2aa04c5e92424
Brendan McNamara <bahai9@live.com>
2014-12-22 14:20:06 -0600

    Adds missing react dependency Adds browserify to the npm start process

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,28 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        
         jshint: {
             all: ['Gruntfile.js', 'public/js/*.js']
         },
+        
         jasmine_node: {
             all: ['public/*']
+        },
+
+        browserify: {
+            dist: {
+                files: {
+                    'public/js/bundle.js': 'public/js/main.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jasmine-node');
+    grunt.loadNpmTasks('grunt-browserify');
 
     grunt.registerTask('test', ['jshint', 'jasmine_node']);
+    grunt.registerTask('launch', ['browserify']);
 };
