c71781c27f8d7674fd43b12e6b77ffe3f5d1cd35
Brendan McNamara <bahai9@live.com>
2015-01-11 20:52:08 -0600

    Update shore framework to provide more sophisticated routing interface

--- public/js/routes.js
+++ public/js/routes.js
@@ -6,64 +6,8 @@
 
 var 
     constants = require('./constants.js'),
-    
     layout = require('./layout'),
-
-    react = require('react'),
-
-    matcher = require('shore').Matcher,
-    router = require('shore').Router,
-
-    defaultRoute = {
-        action: constants.Action.LOAD_NOT_FOUND,
-        component: layout.errorPage.Root
-    },
-
-    routes = [
-        {
-            route: "/",
-            action: constants.Action.LOAD_HOME,
-            component: layout.homeLayout.Root
-        },
-
-        {
-            route: "/course/<courseId>",
-            action: constants.Action.LOAD_COURSE,
-            component: layout.courseLayout.Root
-        },
-
-        {
-            route: "/course/<courseId>/exam/<examId>",
-            action: constants.Action.LOAD_COURSE,
-            component: layout.courseLayout.Root
-        },
-
-        {
-            route: "/course/<courseId>/exam/<examId>/take",
-            action: constants.Action.LOAD_EXAM_RUN,
-            component: layout.examLayout.Root
-        }
-    ],
-
-    layoutMatcher = matcher.createPatternMatcher(),
-
-    onLoadedPage = function () {
-        var component;
-        // Update the path of the layout matcher.
-        layoutMatcher.path(router.path());
-
-        if (!layoutMatcher.resolve()) {
-            // Could not find any routes.
-            component = react.createFactory(defaultRoute.component);
-            react.render(component());
-        }
-    },
-
-    onPageNotFound = function () {
-        var component = react.createFactory(defaultRoute.component);
-        react.render(component());
-    };
-
+    router = require('shore').Router;
 
 module.exports = {
 
@@ -75,42 +19,55 @@ module.exports = {
      */
     config: function () {
 
-        // TODO: Remove any listeners if there are any that already exist.
-        router.on(constants.Event.LOADED_PAGE, onLoadedPage);
-        router.on(constants.Event.PAGE_NOT_FOUND, onPageNotFound);
+        // Add the routes.
 
-        // Configure the layout matcher.
-        layoutMatcher.config({ allowPartialMatch: false });
+        router.routes([
+            {
+                route: "/",
+                action: constants.Action.LOAD_HOME,
+                component: layout.homeLayout.Root
+            },
 
-        // Add the routes from the routes array.
-        routes.forEach(function (routeMap) {
-            if (routeMap.action) {
-                router.registerRoute(routeMap.route, routeMap.action);
+            {
+                route: "/course/<courseId>",
+                action: constants.Action.LOAD_COURSE,
+                component: layout.courseLayout.Root
+            },
+
+            {
+                route: "/course/<courseId>/exam/<examId>",
+                action: constants.Action.LOAD_COURSE,
+                component: layout.courseLayout.Root
+            },
+
+            {
+                route: "/course/<courseId>/exam/<examId>/take",
+                action: constants.Action.LOAD_EXAM_RUN,
+                component: layout.examLayout.Root
             }
-
-            if (routeMap.component) {
-                layoutMatcher.forCase(routeMap.route, function (argMap) {
-                    var component = react.createFactory(routeMap.component);
-                    react.render(component(argMap),
-                                 document.getElementsByTagName('body')[0]);
-                    return true;
-                });
-            }
-
-        });
+        ]);
 
         // Add a default route.
-        router.registerDefaultRoute(defaultRoute.action);
-
-        // Add all the error handling here.
-
-        router.registerError(constants.ErrorType.NO_USER_CREDENTIALS, function () {
-            console.log("No user credentials.");
+        router.defaultRoute({
+            action: constants.Action.LOAD_NOT_FOUND,
+            component: layout.errorPage.Root
         });
 
-        router.registerError(constants.ErrorType.INVALID_EXAM_RUN, router.ErrorOperation.pageNotFound);
+        // Add all the error handling here.
+        router.errors([
+            {
+                errorType: constants.ErrorType.NO_USER_CREDENTIALS,
+                handler: function () {
+                    console.log("No user credentials.");
+                }
+            },
+            {
+                errorType: constants.ErrorType.INVALID_EXAM_RUN,
+                handler: router.ErrorOperation.pageNotFound
+            }
+        ]);
 
-        router.registerDefaultError(function () {
+        router.defaultError(function () {
             console.log("Default error.");
         });
 
