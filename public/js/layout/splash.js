a30489e0bbc51ddc82699f748830a77274bd397d
Brendan McNamara <bahai9@live.com>
2015-01-14 22:18:47 -0600

    Creates design for splash page

--- public/js/layout/splash.js
+++ public/js/layout/splash.js
@@ -21,7 +21,6 @@ var React = require('react'),
                     <headerLayout.Header isOpaque={ true } />
                     <headerLayout.HeaderFill isOpaque={ true } />
                     <SplashFiller />
-                    <FeatureList />
                 </div>
             );
         }
@@ -32,60 +31,21 @@ var React = require('react'),
 
         render: function () {
             return (
-                <div className="splash__filler">
-                    <div className="splash__filler__content">
-                        <p className="splash__filler__catch-phrase">A new way to study!</p>
-                        <div className="splash__filler__button-wrapper">
-                            <button className="button large-button--transparant">
-                                Login
-                            </button>
-                        </div>
-                        <div className="splash__filler__button-wrapper">
-                            <button className="button large-button--transparant">
-                                Sign Up
-                            </button>
-                        </div>
+                <div className="splash__filler__content">
+                    <p className="splash__filler__catch-phrase">A new way to study!</p>
+                    <div className="splash__filler__button-wrapper">
+                        <button className="button large-button--transparant">
+                            Login
+                        </button>
+                    </div>
+                    <div className="splash__filler__button-wrapper">
+                        <button className="button large-button--transparant">
+                            Sign Up
+                        </button>
                     </div>
                 </div>
             );
         }
-    }),
-
-    FeatureList = React.createClass({
-
-        render: function () {
-            return (
-                <div>
-                    <ul className="splash__feature-list">
-                        <li>
-                            <div className="splash__feature-list__item__image-wrapper">
-                                <img src="/img/school.png"></img>
-                            </div>
-                            <div className="splash__feature-list__item__text-wrapper">
-                                <p>Select your school and find your courses.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="splash__feature-list__item__image-wrapper">
-                                <img src="/img/paper.png"></img>
-                            </div>
-                            <div className="splash__feature-list__item__text-wrapper">
-                                <p>Take practice exams and get quick feedback.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="splash__feature-list__item__image-wrapper">
-                                <img src="/img/group.png"></img>
-                            </div>
-                            <div className="splash__feature-list__item__text-wrapper">
-                                <p>Collaborate with friends to create practice question and quiz each other.</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            );
-        }
-
     });
 
 
