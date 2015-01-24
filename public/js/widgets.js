6282c0e7b0a24d6e9253d68a3a349e520745f6d8
Brendan McNamara <bahai9@live.com>
2015-01-23 18:45:35 -0600

    Slight changes to widgets file

--- public/js/widgets.js
+++ public/js/widgets.js
@@ -27,96 +27,101 @@ PieChart = function (context, data) {
     this._data = data;
 };
 
-/**
- * Render the pie chart inside it's context.
- *
- * @method render
- */
-PieChart.prototype.render = function () {
-    var self = this;
-    this._data.forEach(function (segMap, index) {
-        self._drawSegment(index);
-    });
-};
+PieChart.prototype = {
+
+    /**
+     * Render the pie chart inside it's context.
+     *
+     * @method render
+     */
+    render: function () {
+        var self = this;
+        this._data.forEach(function (segMap, index) {
+            self._drawSegment(index);
+        });
+    },
 
 
-/**
- * Get the arc size (in radians) of a segment.
- *
- * @method _segmentArcSize
- * @private
- *
- * @param index {Number} The index of the segment
- *  to get the arc size for.
- *
- * @return {Number} The arc size of the segment
- *  (in radians).
- */
-PieChart.prototype._segmentArcSize = function (index) {
-    var 
-        total = this._data.reduce(function (memo, segMap) {
-            return memo + segMap.value;
-        }, 0),
+    /**
+     * Get the arc size (in radians) of a segment.
+     *
+     * @method _segmentArcSize
+     * @private
+     *
+     * @param index {Number} The index of the segment
+     *  to get the arc size for.
+     *
+     * @return {Number} The arc size of the segment
+     *  (in radians).
+     */
+    _segmentArcSize: function (index) {
+        var 
+            total = this._data.reduce(function (memo, segMap) {
+                return memo + segMap.value;
+            }, 0),
 
-        multiplier = 2 * Math.PI / total;
+            multiplier = 2 * Math.PI / total;
 
-    return this._data[index].value * multiplier;
-};
-
-
-/**
- * The start angle for a partcular segment (in radians).
- *
- * @method _segmentStartAngle
- * @private
- *
- * @param index {Number} The index of the start angle
- *  of the segment.
- *
- * @return {Number} The start angle for the segment (in radians).
- */
-PieChart.prototype._segmentStartAngle = function (index) {
-    var self = this;
-    return this._data.slice(0, index).reduce(function (memo, segMap, i) {
-        return memo + self._segmentArcSize(i);
-    }, 0);
-};
+        return this._data[index].value * multiplier;
+    },
 
 
-/**
- * Draw a segment into the context. The context will be saved and restored
- * to the state prior to when the drawing took place.
- *
- * @method _drawSegment
- * @private
- *
- * @param context {Context} The context of the canvas element.
- *
- * @param index {Number} The index of the element.
- */
-PieChart.prototype._drawSegment = function (index) {
-    var
-        centerX = Math.floor(this._context.canvas.width / 2),
-        centerY = Math.floor(this._context.canvas.height / 2),
-
-        radius = Math.min(this._context.canvas.width / 2, this._context.canvas.height / 2),
-
-        startAngle = this._segmentStartAngle(index),
-        arcSize = this._segmentArcSize(index),
-        endAngle = startAngle + arcSize;
+    /**
+     * The start angle for a partcular segment (in radians).
+     *
+     * @method _segmentStartAngle
+     * @private
+     *
+     * @param index {Number} The index of the start angle
+     *  of the segment.
+     *
+     * @return {Number} The start angle for the segment (in radians).
+     */
+    _segmentStartAngle: function (index) {
+        var self = this;
+        return this._data.slice(0, index).reduce(function (memo, segMap, i) {
+            return memo + self._segmentArcSize(i);
+        }, 0);
+    },
 
 
-    this._context.save();
+    /**
+     * Draw a segment into the context. The context will be saved and restored
+     * to the state prior to when the drawing took place.
+     *
+     * @method _drawSegment
+     * @private
+     *
+     * @param context {Context} The context of the canvas element.
+     *
+     * @param index {Number} The index of the element.
+     */
+    _drawSegment: function (index) {
+        var
+            centerX = Math.floor(this._context.canvas.width / 2),
+            centerY = Math.floor(this._context.canvas.height / 2),
 
-    this._context.beginPath();
-    this._context.moveTo(centerX, centerY);
-    this._context.arc(centerX, centerY, radius, startAngle, endAngle, false);
-    this._context.closePath();
+            radius = Math.min(this._context.canvas.width / 2, this._context.canvas.height / 2),
 
-    this._context.fillStyle = this._data[index].color;
-    this._context.fill();
+            startAngle = this._segmentStartAngle(index),
+            arcSize = this._segmentArcSize(index),
+            endAngle = startAngle + arcSize;
 
-    this._context.restore();
+
+        this._context.save();
+
+        this._context.beginPath();
+        this._context.moveTo(centerX, centerY);
+        this._context.arc(centerX, centerY, radius, startAngle, endAngle, false);
+        this._context.closePath();
+
+        this._context.fillStyle = this._data[index].color;
+        this._context.fill();
+
+        this._context.restore();
+    }
+
+
 };
 
 
